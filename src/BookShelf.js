6289615c0f200af29a2a9984dee8735ccd0caa4f
tasawernawaz <tasawer.nawaz@arbisoft.com>
2020-12-18 19:02:25 +0500

    intriduced state variables to save books in catagories.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,13 +4,12 @@ import BookDetails from './BookDetails'
 
 class BookShelf extends Component {
     render () {
+        const {catagory, books} = this.props
         return (
             <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
+            <h2 className="bookshelf-title">{catagory}</h2>
             <div className="bookshelf-books">
-              <ol className="books-grid">
-                <BookDetails />
-              </ol>
+                <BookDetails books={books}/>
             </div>
           </div>
         )
