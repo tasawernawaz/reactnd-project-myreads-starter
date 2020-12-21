7f5d45a30d35664e6d3dc4a505632a7b8364c27a
tasawernawaz <tasawer.nawaz@arbisoft.com>
2020-12-21 22:06:38 +0500

    fix: fixed warning of using unique key on elements in an array

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -7,7 +7,7 @@ class BookDetails extends Component {
         return (
             <ol className="books-grid">
                 {books.map((book) => (
-                    <li>
+                    <li key={book.id}>
                     <div className="book">
                     <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
